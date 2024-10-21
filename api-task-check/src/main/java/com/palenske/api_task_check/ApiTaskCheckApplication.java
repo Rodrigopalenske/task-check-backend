f46f6a6997157935673bfa1708c10e692fccbd80
Rodrigo Palenske <rodrigopalenske@gmail.com>
2024-10-21 17:10:25 -0300

    Creating the basic structure for the Java API with spring

---
+++ api-task-check/src/main/java/com/palenske/api_task_check/ApiTaskCheckApplication.java
@@ -0,0 +1,13 @@
+package com.palenske.api_task_check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTaskCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTaskCheckApplication.class, args);
+	}
+
+}
---
+++ api-task-check/src/test/java/com/palenske/api_task_check/ApiTaskCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palenske.api_task_check;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTaskCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
